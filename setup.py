180bad4552d23638710e57b5d90258171682951b
Robert James Clay <jame@rocasa.us>
2015-12-13 00:23:56 -0500

    Add a 'long_desciption' entry to the setup.py file.

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from distutils.core import setup
 setup(name='postnews',
       version='0.6',
       description='Post Usenet articles via NNTP from the command line',
+      long_description="""Postnews is used to post Usenet articles (including headers) from
+stdin or a file to a nntp server. The article must at least contain the
+headers 'From:','Newsgroups:', 'Subject:', a newline and a body.""",
       author=['Michael Waschbüsch','Robert James Clay'],
       author_email=['waschbuesch@users.sourceforge.net','jame@rocasa.us'],
       license='GPLv2+',
